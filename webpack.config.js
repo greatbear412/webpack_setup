24154b341f4d3dd25a699fdd83100c6d2c9ff524
shq-hy <13838259284@163.com>
2019-08-03 18:46:15 +0800

    html loader

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
                 }
             },
             {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }, 
+            {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
